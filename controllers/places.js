59285a7e2b834d048138264b9ba1a68fdedfcebf
Dekyi Tenzin <dekyitenzin@Dekyis-Air.lan>
2023-07-11 17:24:27 -0400

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,10 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
+
+router.get('/', (req, res) => {
+    
+  res.render('places/index', { places })
+})
 
 router.get('/new', (req, res) => {
   res.render('places/new')
@@ -6,6 +12,21 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
   console.log(req.body)
+  router.post('/', (req, res) => {
+    console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    res.send('POST /places')
+  })
+  
   res.send('POST /places')
 })
 
@@ -25,10 +46,7 @@ let places = [{
   }]
   
 
-router.get('/', (req, res) => {
-    
-    res.render('places/index', { places })
-})
+
 
  
 
