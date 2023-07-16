a706c782648fcea51610517fd60d07b80fca86ed
Dekyi Tenzin <dekyitenzin@Dekyis-Air.lan>
2023-07-16 13:51:50 -0400

    added edit form

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -4,18 +4,30 @@ const Def = require('../default.jsx')
 function edit_form (data) {
     return (
         <Def>
-         <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
-  <div className="row">
-      <div className="form-group col-sm-6">
-          <label htmlFor="name">Place Name</label>
-          <input className="form-control" id="name" name="name" value={data.place.name} required />
-      </div>
-      <div className="form-group col-sm-6">
-          <label htmlFor="pic">Place Picture</label>
-          <input className="form-control" id="pic" name="pic" value={data.place.pic} />
-      </div>
+         <form method="POST" action={`/places/${data.place.id}`}>
+  <div className="form-group">
+    <label htmlFor="name">Place Name</label>
+    <input className="form-control" id="name" name="name" value={data.place.name} required />
   </div>
+  <div className="form-group">
+    <label htmlFor="pic">Place Picture</label>
+    <input className="form-control" id="pic" name="pic" />
+  </div>
+  <div className="form-group">
+    <label htmlFor="city">City</label>
+    <input className="form-control" id="city" name="city" />
+  </div>
+  <div className="form-group">
+    <label htmlFor="state">State</label>
+    <input className="form-control" id="state" name="state" />
+  </div>
+  <div className="form-group">
+    <label htmlFor="cuisines">Cuisines</label>
+    <input className="form-control" id="cuisines" name="cuisines" required />
+  </div>
+  <input className="btn btn-primary" type="submit" value="Add Place" />
 </form>
+
         </Def>
     )
 }
