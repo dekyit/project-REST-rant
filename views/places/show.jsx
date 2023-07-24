c3b281aa94e25599a970bafaaf2b1585c9e392cb
Dekyi Tenzin <dekyitenzin@dekyis-air.myfiosgateway.com>
2023-07-24 13:29:01 -0400

    Added validations

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -4,25 +4,29 @@ const Def = require("../default");
 function show(data) {
   return (
     <Def>
-      <main>
-        <h1>{data.place.name}</h1>
-        <h2>Rating</h2>
-        <h2>Description</h2>
-        <p>Located in San Diego, Califronia</p>
-        <p>serving modern Thai fusion</p>
+  <main>
+    <div className="row">
+      <div className="col-sm-6">
+        <img src={data.place.pic} alt={data.place.name} />
+        <h3>
+          Located in {data.place.city}, {data.place.state}
+        </h3>
+      </div>
+      <div className="col-sm-6">
+        <h2>
+          Description
+        </h2>
+        <h3>
+          {data.place.showEstablished()}
+        </h3>
+        <h4>
+          Serving {data.place.cuisines}
+        </h4>
+      </div>
+    </div>
+  </main>
+</Def>
 
-        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-          Edit
-        </a>
-        <form method="POST" action={`places/${data.id}?_method=DELETE`}>
-          <button type="submit" className="btn btn-danger">
-            Delete
-          </button>
-        </form>
-
-        <img src="/images/PadThaiDish.jpg" alt="bowl of Pad Thai"></img>
-      </main>
-    </Def>
   );
 }
 
